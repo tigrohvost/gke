693c6cd4042a78dafb4e85dafb1265ca28c02a5e
tigrohvost <30686682+tigrohvost@users.noreply.github.com>
2022-02-04 09:41:18 -0500

    Update back.py

--- back/back.py
+++ back/back.py
@@ -5,7 +5,7 @@ api = Flask(__name__)
 
 @api.route('/random', methods=['GET'])
 def get_random():
-      return str(random.randint(1,1000))
+      return str(random.randint(1,100))
 
 if __name__ == '__main__':
     api.run() 
